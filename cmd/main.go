8a05bba00715323c6b0c0873a53497cd4a69f3e2
Cody Potter <cody.potter@gearbox.com>
2024-11-10 17:09:30 -0500

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"critiqally/views"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	router.HandleFunc("/", indexPageHandler())
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "127.0.0.1:8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
+
+const appTimeout = time.Second * 10
+
+func indexPageHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(context.Background(), appTimeout)
+		defer cancel()
+
+		views.Index().Render(ctx, w)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module critiqally
+
+go 1.23
+
+require (
+	github.com/a-h/templ v0.2.793
+	github.com/gorilla/mux v1.8.1
+)
